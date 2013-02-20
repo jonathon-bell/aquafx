07f9362483f2ad5d829efdaa790e7e8d8715b383
czillmann <claudine.zillmann@gmail.com>
2013-02-20 21:32:37 +0100

    close #6

--- src/main/java/de/zillmann/javafx/aqua/demo/ButtonDemo.java
+++ src/main/java/de/zillmann/javafx/aqua/demo/ButtonDemo.java
@@ -2,10 +2,13 @@ package de.zillmann.javafx.aqua.demo;
 
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
 import javafx.stage.Stage;
 import de.zillmann.javafx.aqua.AquaFx;
 
@@ -13,28 +16,64 @@ public class ButtonDemo extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		final Button b1 = new Button();
-		b1.setText("Abmelden");
-		b1.setDefaultButton(true);
 		BorderPane pane = new BorderPane();
-		pane.setTop(b1);
+		
+		VBox buttonBox = VBoxBuilder.create().padding(new Insets(20, 20, 0, 40))
+				.spacing(10).build();
+		pane.setLeft(buttonBox);
+		
+		Button b1 = new Button();
+		b1.setText("Default");
+		b1.setDefaultButton(true);
+		buttonBox.getChildren().add(b1);
 		
 		Button b2 = new Button();
-		b2.setText("Weitere Optionen ...");
+		b2.setText("Default");
 		b2.setDisable(true);
-		pane.setLeft(b2);
+		b2.setDefaultButton(true);
+		buttonBox.getChildren().add(b2);
 		
-		Button b = new Button("Abbrechen");
-		pane.setBottom(b);
+		Button b3 = new Button();
+		b3.setText("Normal");
+		buttonBox.getChildren().add(b3);
 		
-		CheckBox box1 = new CheckBox("Checkbox 1");
-		box1.setSelected(true);
-		pane.setCenter(box1);
+		Button b4 = new Button();
+		b4.setText("Normal");
+		b4.setDisable(true);
+		buttonBox.getChildren().add(b4);
 		
-		CheckBox box2 = new CheckBox("Checkbox 2");
-		pane.setRight(box2);
 		
-		Scene myScene = new Scene(pane);
+		VBox checkBoxBox = VBoxBuilder.create().padding(new Insets(20, 20, 0, 40))
+				.spacing(10).build();
+		pane.setRight(checkBoxBox);
+
+		
+		CheckBox box1 = new CheckBox("Normal");
+		checkBoxBox.getChildren().add(box1);
+		
+		CheckBox box2 = new CheckBox("Normal");
+		box2.setDisable(true);
+		checkBoxBox.getChildren().add(box2);
+		
+		CheckBox box3 = new CheckBox("Selected");
+		box3.setSelected(true);
+		checkBoxBox.getChildren().add(box3);
+
+		CheckBox box4 = new CheckBox("Selected");
+		box4.setSelected(true);
+		box4.setDisable(true);
+		checkBoxBox.getChildren().add(box4);
+		
+		CheckBox box5 = new CheckBox("Indeterminate");
+		box5.setIndeterminate(true);
+		checkBoxBox.getChildren().add(box5);
+
+		CheckBox box6 = new CheckBox("Indeterminate");
+		box6.setIndeterminate(true);
+		box6.setDisable(true);
+		checkBoxBox.getChildren().add(box6);
+		
+		Scene myScene = new Scene(pane, 300, 200);
 		AquaFx.style(myScene);
 		
 		stage.setScene(myScene);
