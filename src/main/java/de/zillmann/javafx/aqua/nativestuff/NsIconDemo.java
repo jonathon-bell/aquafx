4a54aaf46336a43d9236d219da03f9a6a867e0e6
czillmann <claudine.zillmann@gmail.com>
2013-03-03 16:52:40 +0100

    native stuff

---
+++ src/main/java/de/zillmann/javafx/aqua/nativestuff/NsIconDemo.java
@@ -0,0 +1,26 @@
+package de.zillmann.javafx.aqua.nativestuff;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class NsIconDemo extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FlowPane pane = new FlowPane();
+		for (NsImageIcon nsImageIcon : NsImageIcon.values()) {
+			pane.getChildren().add(new ImageView(NsImageIconLoader.load(nsImageIcon)));
+		}
+		
+		Scene myScene = new Scene(pane);
+		primaryStage.setScene(myScene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
