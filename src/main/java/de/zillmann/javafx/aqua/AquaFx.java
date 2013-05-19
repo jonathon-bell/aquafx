c89d7e10f6b6254f03bc1c43b25659fef151288d
czillmann <claudine.zillmann@gmail.com>
2013-05-19 21:13:01 +0200

    #51

--- src/main/java/de/zillmann/javafx/aqua/AquaFx.java
+++ src/main/java/de/zillmann/javafx/aqua/AquaFx.java
@@ -1,18 +1,29 @@
 package de.zillmann.javafx.aqua;
 
 import javafx.application.Application;
-import javafx.scene.control.Button;
 import javafx.scene.control.Control;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import de.zillmann.javafx.aqua.controls.skin.styles.ButtonType;
 import de.zillmann.javafx.aqua.controls.skin.styles.ControlSizeVariant;
-import de.zillmann.javafx.aqua.controls.skin.styles.MacOSDefaultIcons;
 import de.zillmann.javafx.aqua.controls.skin.styles.StyleFactory;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ButtonStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.CheckBoxStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ChoiceBoxStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ColorPickerStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ComboBoxStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.LabelStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.PasswordFieldStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ProgressBarStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ProgressIndicatorStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.RadioButtonStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ScrollBarStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.SliderStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.TabPaneStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.TextAreaStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.TextFieldStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ToggleButtonStyler;
+import de.zillmann.javafx.aqua.controls.skin.styles.styler.ToolBarStyler;
 
 public class AquaFx {
 
@@ -24,55 +35,112 @@ public class AquaFx {
         Application.setUserAgentStylesheet(AQUA_CSS_NAME);
     }
 
-    public static void skin(Button button, ControlSizeVariant controlSizeVariant) {
-        skin(button, ButtonType.DEFAULT, controlSizeVariant);
-    }
-
-    public static void skin(Button button, ButtonType buttonType) {
-        skin(button, buttonType, ControlSizeVariant.REGULAR);
-    }
-
-    public static void skin(Button button, ButtonType buttonType, ControlSizeVariant controlSizeVariant) {
-        styleFactory.addStyles(button, buttonType, controlSizeVariant);
+    public static void styleStage(Stage stage, StageStyle style) {
+        stage.initStyle(style);
     }
 
     public static <T extends Control> void resizeControl(T control, ControlSizeVariant controlSizeVariant) {
         styleFactory.addStyles(control, controlSizeVariant);
     }
 
-    public static void setButtonIcon(Button button, MacOSDefaultIcons icon) {
-        Region svgIcon = new Region();
-        svgIcon.getStyleClass().add(icon.getStyleName());
-        svgIcon.getStyleClass().add("aquaicon");
-        button.setGraphic(svgIcon);
-        button.getStyleClass().add("button-"+icon.getStyleName());
-    }
-
-    public static void setShareButton(Button button) {
-        StackPane stack = new StackPane();
-        String iconBase = MacOSDefaultIcons.SHARE.getStyleName();
-        stack.getStyleClass().add("aquaicon");
-
-        Region svgIcon = new Region();
-        svgIcon.getStyleClass().add(iconBase + "-square");
-        stack.getChildren().add(svgIcon);
-
-        Region svgIcon2 = new Region();
-        svgIcon2.getStyleClass().add(iconBase + "-arrow");
-        stack.getChildren().add(svgIcon2);
-
-        button.setGraphic(stack);
-    }
-
-    public static void styleSearchField(TextField textField, MacOSDefaultIcons icon) {
-        textField.getStyleClass().add(icon.getStyleName());
-    }
+    // public static void skin(Button button, ControlSizeVariant controlSizeVariant) {
+    // skin(button, ButtonType.DEFAULT, controlSizeVariant);
+    // }
+    //
+    // public static void skin(Button button, ButtonType buttonType) {
+    // skin(button, buttonType, ControlSizeVariant.REGULAR);
+    // }
+    //
+    // public static void skin(Button button, ButtonType buttonType, ControlSizeVariant
+    // controlSizeVariant) {
+    // ButtonStyler.create().setSizeVariant(controlSizeVariant).setType(buttonType).style(button);
+    // }
+    //
+    // public static void setButtonIcon(Button button, MacOSDefaultIcons icon) {
+    // ButtonStyler.create().setIcon(icon).style(button);
+    // }
+    //
+    // public static void setShareButton(Button button) {
+    // ButtonStyler.create().setIcon(MacOSDefaultIcons.SHARE).style(button);
+    // }
+    //
+    // public static void styleSearchField(TextField textField) {
+    // textField.getStyleClass().add(MacOSDefaultIcons.SEARCH.getStyleName());
+    // }
 
     public static void setGroupBox(Pane pane) {
         pane.getStyleClass().add("aqua-group-box");
     }
 
-    public static void styleStage(Stage stage, StageStyle style) {
-        stage.initStyle(style);
+    /***************************************************************
+     * * StylerClasses for skinned Controls * *
+     **************************************************************/
+
+    public static ButtonStyler createButtonStyler() {
+        return ButtonStyler.create();
+    }
+
+    public static ToggleButtonStyler createToggleButtonStyler() {
+        return ToggleButtonStyler.create();
+    }
+
+    public static LabelStyler createLabelStyler() {
+        return LabelStyler.create();
+    }
+
+    public static TextFieldStyler createTextFieldStyler() {
+        return TextFieldStyler.create();
+    }
+
+    public static TextAreaStyler createTextAreaStyler() {
+        return TextAreaStyler.create();
+    }
+
+    public static PasswordFieldStyler createPasswordFieldStyler() {
+        return PasswordFieldStyler.create();
+    }
+
+    public static CheckBoxStyler createCheckBoxStyler() {
+        return CheckBoxStyler.create();
+    }
+
+    public static RadioButtonStyler createRadioButtonStyler() {
+        return RadioButtonStyler.create();
+    }
+
+    public static ChoiceBoxStyler createChoiceBoxStyler() {
+        return ChoiceBoxStyler.create();
+    }
+
+    public static ComboBoxStyler createComboBoxStyler() {
+        return ComboBoxStyler.create();
+    }
+
+    public static ColorPickerStyler createColorPickerStyler() {
+        return ColorPickerStyler.create();
+    }
+
+    public static ProgressBarStyler createProgressBarStyler() {
+        return ProgressBarStyler.create();
+    }
+
+    public static ProgressIndicatorStyler createProgressIndicatorStyler() {
+        return ProgressIndicatorStyler.create();
+    }
+
+    public static SliderStyler createSliderStyler() {
+        return SliderStyler.create();
+    }
+
+    public static ScrollBarStyler createScrollBarStyler() {
+        return ScrollBarStyler.create();
+    }
+
+    public static TabPaneStyler createTabPaneStyler() {
+        return TabPaneStyler.create();
+    }
+
+    public static ToolBarStyler createToolBarStyler() {
+        return ToolBarStyler.create();
     }
 }
